5e043d4f4cebf8167915097ba0170e85ae4208d7
StroboBrain <parkschaden@gmail.com>
2024-12-03 15:02:45 +0100

    added BasiTasksFeatureModel from local workspace

--- BasicModelImplementation/src/TaskCreation/TaskCreation.java
+++ BasicModelImplementation/src/TaskCreation/TaskCreation.java
@@ -1,14 +1,10 @@
-package TaskCreation; 
-import java.util.Random; 
-
-import java.util.ArrayList; 
-import PrimeUtility.*; 
-
-
 /**
- * Creates Multiplication Solutions
+ * TODO description
  */
 
+package TaskCreation; 
+
+import java.util.Random; 
 
 public   class  TaskCreation {
 	
@@ -24,45 +20,16 @@ public   class  TaskCreation {
 
 	
 	
-
 	private int chooseSolution  (int rangeMin, int rangeMax) {
-		//Incresing the minimun so that there is a possible solution
-		int range = rangeMax-rangeMin;
-		int possibleSolution = rangeMin + this.random.nextInt(range);
-		// Change possibleSolution if too small or too big
-		if (possibleSolution<rangeMin+ range / 10){
-			possibleSolution += range / 10;
-		}
-		else if (possibleSolution> rangeMax - range / 10) {
-			possibleSolution -= range / 10;
-		}
-		
-		for (int i = possibleSolution; i<=rangeMax;i++) {
-			if (!this.primeUtility.isPrime(i)) {
-				return i;
-			}
-		}
-		for (int i = possibleSolution;i>=rangeMin;i--) {
-			if (!this.primeUtility.isPrime(possibleSolution-i)) {
-				return possibleSolution-i;
-			}
-		}
-		assert false : "A solution should always be found";
-		return -10101;
+		return rangeMin + random.nextInt((rangeMax - rangeMin) + 1); 
 	}
 
 	
 	
- 
+
 	  public String createTask  (int rangeMin, int rangeMax, int solution) {
-		 ArrayList<Integer> factorList = this.primeUtility.factors(solution);
-		 int index = random.nextInt(factorList.size());
-		 assert index>=0 : "Not working index" + index;
-		 int firstFactor = factorList.get(index);
-		 int secondeFactor = (int) solution/firstFactor;
-		 assert firstFactor * secondeFactor == solution;
-		 return firstFactor + "*" + secondeFactor + "=";
-	       
+	        int summand = rangeMin + random.nextInt((solution - rangeMin) + 1);
+	        return summand + "+" + (solution - summand);
 	    }
 
 	
@@ -87,9 +54,6 @@ public   class  TaskCreation {
 	public int getSolution() {
 		return this.solution;
 	}
-
-	
-	private PrimeUtility primeUtility = new PrimeUtility();
 
 
 }
---
+++ BasicTasksFeatureModel/README.md
@@ -0,0 +1,8 @@
+# BasicTaskFeatureModel
+
+### BasicTask
+Basic tasks are of following type:
+arithmetical expression = 
+
+### Cross Constraints
+The ranges are not explicit implemented, this is the reason some cross constraints are absent.
